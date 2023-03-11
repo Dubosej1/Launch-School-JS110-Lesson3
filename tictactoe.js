efeae9636b278f2d64716f1491eacc781f958ec9
Johnaven Dubose <johnavenfreelance@gmail.com>
2023-03-11 04:55:48 +0000

    Refactor executeRound() and detectWinner() to reduce line count in code.

--- tictactoe.js
+++ tictactoe.js
@@ -79,32 +79,27 @@ function determineInitialPlayer() {
 function executeRound(currentPlayer) {
   let winner;
 
+  let board = initializeBoard();
+
   while (true) {
-    let board = initializeBoard();
-
-    while (true) {
-      displayBoard(board);
-
-      chooseSquare(board, currentPlayer);
-      currentPlayer = alternatePlayer(currentPlayer);
-
-      if (someoneWon(board) || boardFull(board))  break;
-    }
-
     displayBoard(board);
 
+    chooseSquare(board, currentPlayer);
+    currentPlayer = alternatePlayer(currentPlayer);
 
-    if (someoneWon(board)) {
-      winner = detectWinner(board);
-    } else {
-      winner = "Tie";
-    }
-
-    displayRoundResult(winner);
-    freezeGame();
-
-    break;
+    if (someoneWon(board) || boardFull(board))  break;
   }
+
+  displayBoard(board);
+
+  if (someoneWon(board)) {
+    winner = detectWinner(board);
+  } else {
+    winner = "Tie";
+  }
+
+  displayRoundResult(winner);
+  freezeGame();
 
   return winner;
 }
@@ -313,24 +308,19 @@ function someoneWon(board) {
 }
 
 function detectWinner(board) {
-  let winningLines = [
-    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
-    [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
-    [1, 5, 9], [3, 5, 7]             // diagonals
-  ];
+  let winningLines = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
   for (let line = 0; line < winningLines.length; line++) {
     let [ sq1, sq2, sq3 ] = winningLines[line];
 
     if (
-      board[sq1] === HUMAN_MARKER &&
-        board[sq2] === HUMAN_MARKER &&
+      board[sq1] === HUMAN_MARKER && board[sq2] === HUMAN_MARKER &&
         board[sq3] === HUMAN_MARKER
     ) {
       return 'Player';
     } else if (
-      board[sq1] === COMPUTER_MARKER &&
-        board[sq2] === COMPUTER_MARKER &&
+      board[sq1] === COMPUTER_MARKER && board[sq2] === COMPUTER_MARKER &&
         board[sq3] === COMPUTER_MARKER
     ) {
       return 'Computer';
